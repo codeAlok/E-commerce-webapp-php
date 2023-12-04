630d3d1ca6379c127ed126bd6cb7451dc46c7cd8
alok kumar <alok60541@gmail.com>
2023-12-04 01:51:26 -0800

    initial home page

---
+++ index.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- external css link -->
+    <link rel="stylesheet" href="css/style.css">
+
+
+    <!-- font awesome link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <!-- <h1>it's working</h1> -->
+    <!-- navbar -->
+    <div class="header">
+        <a href="#" class="logo">LOGO</a>
+        <nav class="navbar">
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="#">Products</a>
+                </li>
+                <li>
+                    <a href="#">Register</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+                <li>
+                    <a href="#"><i class="fa fa-shopping-cart" aria-hidden="true"></i><sup>1</sup></a>
+                </li>
+                <li>
+                    <a href="#">Total price: 100/-</a>
+                </li>
+            </ul>
+            <form action="#">
+                <input type="search" placeholder="Search" aria-label="Search">
+                <button class="btn" type="submit">Search</button>
+            </form>
+        </nav>
+    </div>
+
+    <!-- second child -->
+    <nav class="navbar header">
+        <ul>
+            <li>
+                <a href="#">Welcome Guest</a>
+            </li>
+            <li>
+                <a href="#">Login</a>
+            </li>
+        </ul>
+    </nav>
+
+    <!-- third child -->
+    <div class="div3">
+        <h3 class="text-center">Hidden Store</h3>
+        <p class="text-center">Communication is at the heart of e-commerce and community</p>
+    </div>
+
+    <!-- fourth child -->
+    <div class="container">
+        <!-- sidenav -->
+        <div class="sidenav">
+            <!-- brands section -->
+            <ul>
+                <li>
+                    <a href="#"><h4>Brands</h4></a>
+                </li>
+                <li>
+                    <a href="#">brand1</a>
+                </li>
+                <li>
+                    <a href="#">brand2</a>
+                </li>
+                <li>
+                    <a href="#">brand3</a>
+                </li>
+                <li>
+                    <a href="#">brand4</a>
+                </li>
+                <li>
+                    <a href="#">brand5</a>
+                </li>
+                <li>
+                    <a href="#">brand5</a>
+                </li>
+            </ul>
+
+            <!-- categories section -->
+            <ul>
+                <li>
+                    <a href="#"><h4>categories</h4></a>
+                </li>
+                <li>
+                    <a href="#">categories1</a>
+                </li>
+                <li>
+                    <a href="#">categories2</a>
+                </li>
+                <li>
+                    <a href="#">categories3</a>
+                </li>
+                <li>
+                    <a href="#">categories4</a>
+                </li>
+                <li>
+                    <a href="#">categories5</a>
+                </li>
+                <li>
+                    <a href="#">categories5</a>
+                </li>
+            </ul>
+        </div>
+
+        <!-- products -->
+        <div class="product-area">
+            <div class="card">
+                <img src="images/men_shoe1.jpg" alt="product1">
+                <div class="card-body">
+                    <h5>card title</h5>
+                    <p>some text related to products</p>
+                    <a href="#" class="btn">Add to cart</a>
+                    <a href="#" class="btn">buy now</a>
+
+                </div>
+            </div>
+            <div class="card">
+                <img src="images/men_shoe2.jpg" alt="product1">
+                <div class="card-body">
+                    <h5>card title</h5>
+                    <p>some text related to products</p>
+                    <a href="#" class="btn">Add to cart</a>
+                    <a href="#" class="btn">buy now</a>
+
+                </div>
+            </div>
+            <div class="card">
+                <img src="images/women_shoe1.jpg" alt="product1">
+                <div class="card-body">
+                    <h5>card title</h5>
+                    <p>some text related to products</p>
+                    <a href="#" class="btn">Add to cart</a>
+                    <a href="#" class="btn">buy now</a>
+
+                </div>
+            </div>
+            <div class="card">
+                <img src="images/men_shoe2.jpg" alt="product1">
+                <div class="card-body">
+                    <h5>card title</h5>
+                    <p>some text related to products</p>
+                    <a href="#" class="btn">Add to cart</a>
+                    <a href="#" class="btn">buy now</a>
+
+                </div>
+            </div>
+            <div class="card">
+                <img src="images/women_shoe1.jpg" alt="product1">
+                <div class="card-body">
+                    <h5>card title</h5>
+                    <p>some text related to products</p>
+                    <a href="#" class="btn">Add to cart</a>
+                    <a href="#" class="btn">buy now</a>
+
+                </div>
+            </div>
+            <div class="card">
+                <img src="images/men_shoe2.jpg" alt="product1">
+                <div class="card-body">
+                    <h5>card title</h5>
+                    <p>some text related to products</p>
+                    <a href="#" class="btn">Add to cart</a>
+                    <a href="#" class="btn">buy now</a>
+
+                </div>
+            </div>
+            <div class="card">
+                <img src="images/men_shoe11.jpg" alt="product1">
+                <div class="card-body">
+                    <h5>card title</h5>
+                    <p>some text related to products</p>
+                    <a href="#" class="btn">Add to cart</a>
+                    <a href="#" class="btn">buy now</a>
+
+                </div>
+            </div>
+            <div class="card">
+                <img src="images/men_shoe3.jpg" alt="product1">
+                <div class="card-body">
+                    <h5>card title</h5>
+                    <p>some text related to products</p>
+                    <a href="#" class="btn">Add to cart</a>
+                    <a href="#" class="btn">buy now</a>
+
+                </div>
+            </div>
+            <div class="card">
+                <img src="images/men_shoe10.jpg" alt="product1">
+                <div class="card-body">
+                    <h5>card title</h5>
+                    <p>some text related to products</p>
+                    <a href="#" class="btn">Add to cart</a>
+                    <a href="#" class="btn">buy now</a>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+    <!-- footer -->
+    <footer class="footer">
+        copyright @ all rights reserved
+    </footer>
+</body>
+
+</html>
