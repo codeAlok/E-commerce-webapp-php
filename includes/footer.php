1e14be21c0724d9876aa1445183542792429f357
alok kumar <alok60541@gmail.com>
2024-01-06 11:16:55 +0530

    footer content

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,50 @@
 
 
 <footer class="footer">
-        copyright @ all rights reserved
+        <div class="footer-container">
+                <section class="sec slogan">
+                        <h2><a href="#" class="logo">Heel2Toe</a></h2>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum soluta aliquam eos doloribus magni! Soluta odio sed repellat possimus minima?</p>
+
+                        <ul class="social-link">
+                                <li class="#"><a href="#"><i class="fa-brands fa-square-facebook"></a></i></li>
+                                <li class="#"><a href="#"><i class="fa-brands fa-square-x-twitter"></a></i></li>
+                                <li class="#"><a href="#"><i class="fa-brands fa-square-instagram"></a></i></li>
+                                <li class="#"><a href="#"><i class="fa-brands fa-linkedin"></a></i></li>
+                        </ul>
+                </section>
+                <section class="sec quicklinks">
+                        <h2>Support</h2>
+                        <ul>
+                                <li><a href="#">FAQ</a></li>
+                                <li><a href="#">Privacy Policy</a></li>
+                                <li><a href="#">Help</a></li>
+                                <li><a href="#">Contact</a></li>
+                        </ul>
+                </section>
+                <section class="sec quicklinks">
+                        <h2>Shop</h2>
+                        <ul>
+                                <li><a href="#">Men</a></li>
+                                <li><a href="#">Women</a></li>
+                                <li><a href="#">Kids</a></li>
+                                <li><a href="#">shoes</a></li>
+                        </ul>
+                </section>
+                <section class="sec contact-info">
+                        <h2>Contact Us</h2>
+                        <ul class="info">
+                        <li><span><i class="fa-solid fa-phone"></i></span> <p><a href="#">+91 935 123 1243</a></p>
+                                </li>
+                        <li><span><i class="fa-solid fa-envelope"></i></span> <p><a href="#">knowmore@gmail.com</a></p>
+                                </li>
+                        <li><span><i class="fa-solid fa-location-dot"></i></i></span> <p><a href="#">new road deoghar lane near college, jharkhand, india</a></p>
+                                </li>
+                        </ul>
+                </section>   
+        </div>
+        
+        <div class="copyrightText">
+                <p>copyright 2024 @ all rights reserved</p>
+        </div>
 </footer>
