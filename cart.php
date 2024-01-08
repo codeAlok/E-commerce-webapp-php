6e1e178c6bebb1d8e74a1896003dd5c542268c33
alok kumar <alok60541@gmail.com>
2024-01-09 05:21:55 +0530

    checkout and cart-detail page design done

--- cart.php
+++ cart.php
@@ -17,6 +17,7 @@
 
     <!-- external css link -->
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/checkout.css">
 
 
     <!-- font awesome link -->
@@ -61,20 +62,10 @@
 
     <!-- ************ cart item details show ********* -->
     <section class="cart-detail-area">
-        <div class="row">
+        <div class="cart-table">
             <form action="" method="post">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Product Title</th>
-                            <th>Product image</th>
-                            <th>quantity</th>
-                            <th>Total Price</th>
-                            <th>Remove</th>
-                            <th>Operations</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+                <table border="2">
+                    
                         <!-- php code to display dynamic data -->
                         <?php
                              global $con;  // to access globally
@@ -82,53 +73,225 @@
                             $total_price = 0;
                             $cart_query = "select * from cart_details where ip_address='$get_ip_address'";
                             $result = mysqli_query($con, $cart_query);
-                    
-                            // to get price according to product_id having that ip_address from products table for each product
-                            while($row = mysqli_fetch_array($result)) {
-                                $product_id = $row['product_id']; // product_id from cart_details table
-                                $select_products = "select * from products where product_id=$product_id";
-                                $result_products = mysqli_query($con, $select_products);
-                    
-                                while($row_product_price = mysqli_fetch_array($result_products)) {
-                                    $product_price = array($row_product_price['product_price']); // [100, 1050, 500, ...]
 
-                                    $price_table = $row_product_price['product_price'];
-                                    $product_title = $row_product_price['product_title'];
-                                    $product_image1 = $row_product_price['product_image1'];
+                            $result_count = mysqli_num_rows($result);
 
-                                    $product_values = array_sum($product_price); // [100 + 1050 + 500] = [1650]
-                                    $total_price += $product_values; // 100 + 1050 + 500 ...
-                                    
-                                    echo "<tr>
-                                        <td>$product_title</td>
-                                        <td><img src='./admin_panel/product_images/$product_image1' alt='image1' width='140px'></td>
-                                        <td><input type='number' name='quantity'></td>
-                                        <td>₹ $price_table/-</td>
-                                        <td><input type='checkbox'></td>
-                                        <td>
-                                        <input type='submit' value='Update Cart' name='update_cart'>
-                                        <button>Remove</button>
-                                        </td>
-                                        </tr>";
-                                        if(isset($_POST['update_cart'])) {
-                                            $quantities = $_POST['quantity'];
-                                            $update_cart = "update cart_details set quantity=$quantities where ip_address='$get_ip_address'";
-                                            $result_products_quantity = mysqli_query($con, $update_cart);
-                                            $total_price = $total_price * $quantities;
-                                        }
-                                }    
-                            } 
+                            if($result_count > 0) {
+                                echo "<thead>
+                                        <tr>
+                                            <th>Product Title</th>
+                                            <th>Product image</th>
+                                            <th>quantity</th>
+                                            <th>Total Price</th>
+                                            <th>Remove</th>
+                                            <th>Operations</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>";
+                            
+                    
+                                // to get price according to product_id having that ip_address from products table for each product
+                                while($row = mysqli_fetch_array($result)) {
+                                    $product_id = $row['product_id']; // product_id from cart_details table
+                                    $select_products = "select * from products where product_id=$product_id";
+                                    $result_products = mysqli_query($con, $select_products);
+                        
+                                    while($row_product_price = mysqli_fetch_array($result_products)) {
+                                        $product_price = array($row_product_price['product_price']); // [100, 1050, 500, ...]
+
+                                        $price_table = $row_product_price['product_price'];
+                                        $product_title = $row_product_price['product_title'];
+                                        $product_image1 = $row_product_price['product_image1'];
+
+                                        $product_values = array_sum($product_price); // [100 + 1050 + 500] = [1650]
+                                        $total_price += $product_values; // 100 + 1050 + 500 ...
+                                        
+                                        echo "<tr>
+                                            <td>$product_title</td>
+                                            <td><img src='./admin_panel/product_images/$product_image1' alt='image1' width='140px'></td>
+                                            <td><input type='number' name='quantity'></td>
+                                            <td>₹ $price_table/-</td>
+                                            <td><input type='checkbox' name='removeitem[]' value='$product_id'></td>
+                                            <td>
+                                            <input type='submit' value='Update Cart' name='update_cart' class='update-cart'>
+                                            <input type='submit' value='Remove Cart' name='remove_cart' class='remove-cart'>
+                                            </td>
+                                            </tr>";
+                                            if(isset($_POST['update_cart'])) {
+                                                $quantities = $_POST['quantity'];
+                                                $update_cart = "update cart_details set quantity=$quantities where ip_address='$get_ip_address'";
+                                                $result_products_quantity = mysqli_query($con, $update_cart);
+                                                $total_price = $total_price * $quantities;
+                                            }
+                                    }    
+                                } 
+                            }
+                            else {
+                                echo "<h2> Cart is empty</h2>";
+                            }
+                            
                         ?>
                     </tbody>
                 </table>
             </form>               
             <!-- total price -->
-            <div>
-                <h4>Subtotal: <?php echo "₹ $total_price" ?>/-</h4>\
-                <a href="#"><button>Checkout</button></a>
+            <div class='total'>
+                <h4>Subtotal: <?php echo "₹ $total_price" ?>/-</h4>
             </div>
         </div>
     </section>
+
+    <!-- ****** checkout page section from here  ******-->
+    <h2>Checkout Details</h2>
+    <!-- Three parts inside this div -->
+    <div class="row" id="checkout"> 
+      <div class="col-75">
+        <div class="container">
+          <!-- Both Form are combined one Form -->
+          <form action="/checkout.php">
+
+          <!-- Delivery and Payment Form both present inside this div -->
+            <div class="row">
+              <!-- div for Delivery Form -->
+              <div class="col-50">
+                <h3>Delivery Address</h3>
+                <label for="fname"><i class="fa-solid fa-user"></i> Full Name</label>
+                <input
+                  type="text"
+                  id="fname"
+                  name="firstname"
+                  placeholder="Enter Name"
+                />
+                <label for="email"><i class="fa fa-envelope"></i> Email</label>
+                <input
+                  type="text"
+                  id="email"
+                  name="email"
+                  placeholder="Enter Email"
+                />
+                <label for="adr"
+                  ><i class="fa-regular fa-address-card"></i> Address</label
+                >
+                <input
+                  type="text"
+                  id="adr"
+                  name="address"
+                  placeholder="Enter Address"
+                />
+                <label for="city"><i class="fa-solid fa-city"></i> City</label>
+                <input
+                  type="text"
+                  id="city"
+                  name="city"
+                  placeholder="Deoghar"
+                />
+
+                <!-- This div row is only for Zip and State -->
+                <div class="row">
+                  <div class="col-50">
+                    <label for="state">State</label>
+                    <input
+                      type="text"
+                      id="state"
+                      name="state"
+                      placeholder="Jharkhand"
+                    />
+                  </div>
+                  <div class="col-50">
+                    <label for="zip">Zip</label>
+                    <input
+                      type="text"
+                      id="zip"
+                      name="zip"
+                      placeholder="814112"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <!-- Payment Form inside this div -->
+              <div class="col-50">
+                <h3>Payment</h3>
+                <label for="fname">Accepted Cards</label>
+                <div class="icon-container">
+                  <i class="fa fa-cc-visa" style="color: navy"></i>
+                  <i class="fa fa-cc-mastercard" style="color: red"></i>
+                  <i class="fa-brands fa-cc-amazon-pay" style="color: rgb(235, 235, 36)"></i>
+                  <i class="fa-brands fa-cc-paypal" style="color: rgb(11, 186, 235)"></i>
+                  <i class="fa fa-cc-amex" style="color: blue"></i>
+                  <i class="fa fa-cc-discover" style="color: orange"></i>
+                </div>
+                <label for="cname">Cardholder name</label>
+                <input
+                  type="text"
+                  id="cname"
+                  name="cardname"
+                  placeholder="Rishav"
+                />
+                <label for="ccnum">Credit / Debit card number</label>
+                <input
+                  type="text"
+                  id="ccnum"
+                  name="cardnumber"
+                  placeholder="1111-2222-3333-4444"
+                />
+                <label for="expmonth">Expiry Month</label>
+                <input
+                  type="text"
+                  id="expmonth"
+                  name="expmonth"
+                  placeholder="October"
+                />
+                <div class="row">
+                  <div class="col-50">
+                    <label for="expyear">Expiry Year</label>
+                    <input
+                      type="text"
+                      id="expyear"
+                      name="expyear"
+                      placeholder="2017"
+                    />
+                  </div>
+                  <div class="col-50">
+                    <label for="cvv">CVV</label>
+                    <input type="text" id="cvv" name="cvv" placeholder="100" />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <label>
+              <input type="checkbox" checked="checked" name="sameadr" />
+              Delivery address same as billing
+            </label>
+            <input type="submit" value="Continue to checkout" class="btn" />
+          </form>
+        </div>
+      </div>
+      
+    </div>
+
+    <!-- function to remove items -->
+    <?php
+        function remove_cart_item() {
+            global $con;
+
+            if(isset($_POST['remove_cart'])) {
+                foreach($_POST['removeitem'] as $remove_id) {
+                    echo $remove_id;
+
+                    $delete_query = "delete from cart_details where product_id=$remove_id";
+                    $run_delete = mysqli_query($con, $delete_query);
+
+                    if($run_delete) {
+                        echo "<script>window.open('cart.php','_self')</script>";
+                    }
+                }
+            }
+        }
+
+        echo $remove_item = remove_cart_item();
+
+    ?>
 
     <!-- include footer -->
     <?php include("./includes/footer.php");  ?>
